27a00aaf79ce9ae89a70d7603201b9104e8d7a3d
SunJun <i@sjis.me>
2018-08-06 18:29:04 +0800

    1

--- src/github.com/sjatsh/algorithms/sort/insertion.go
+++ src/github.com/sjatsh/algorithms/sort/insertion.go
@@ -2,18 +2,26 @@ package main
 
 import "fmt"
 
+/**
+一般来说，插入排序都采用in-place在数组上实现。具体算法描述如下：
+
+1. 从第一个元素开始，该元素可以认为已经被排序；
+2. 取出下一个元素，在已经排序的元素序列中从后向前扫描；
+3. 如果该元素（已排序）大于新元素，将该元素移到下一位置；
+4. 重复步骤3，直到找到已排序的元素小于或者等于新元素的位置；
+5. 将新元素插入到该位置后；
+6. 重复步骤2~5。
+*/
 func main() {
 
-	count := 0
+	//https://images2017.cnblogs.com/blog/849589/201710/849589-20171015225645277-1151100000.gif
 	var preIndex, current int
 	array := []int{6, 7, 9, 3, 6, 8, 1, 9, 3}
 
 	for i := 1; i < len(array); i++ {
-		count++
 		preIndex = i - 1
 		current = array[i]
 		for preIndex >= 0 && array[preIndex] > current {
-			count++
 			array[preIndex+1] = array[preIndex]
 			preIndex--
 		}
@@ -21,5 +29,4 @@ func main() {
 	}
 
 	fmt.Println(array)
-	fmt.Printf("count:%d\n", count)
 }
--- src/github.com/sjatsh/algorithms/sort/shell.go
+++ src/github.com/sjatsh/algorithms/sort/shell.go
@@ -4,19 +4,16 @@ import "fmt"
 
 func main() {
 
-	count := 0
-	array := []int{4, 3, 5, 6}
+	//https://images2018.cnblogs.com/blog/849589/201803/849589-20180331170017421-364506073.gif
+	array := []int{6, 7, 9, 3, 6, 8, 1, 9, 3}
 	n := len(array)
 	var gap, i, j int
 
 	for gap = n / 2; gap > 0; gap /= 2 {
-		count++
 		//插入排序简洁写法
 		for i = gap; i < n; i++ {
-			count++
 			num := array[i]
 			for j = i - gap; j >= 0 && array[j] > num; j -= gap {
-				count++
 				array[j+gap] = array[j]
 			}
 			array[j+gap] = num
@@ -24,5 +21,4 @@ func main() {
 	}
 
 	fmt.Println(array)
-	fmt.Printf("count:%d\n", count)
 }
