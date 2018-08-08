cba03aee2dee3689e15a74d254c39b0b2481d0c7
SunJun <i@sjis.me>
2018-08-08 17:43:09 +0800

    1

--- src/github.com/sjatsh/algorithms/sort/shell.go
+++ src/github.com/sjatsh/algorithms/sort/shell.go
@@ -2,9 +2,14 @@ package main
 
 import "fmt"
 
+/**
+1 设置gap序列即增量序列，最后一次gap必须是1
+2 将相距gap的一组数按照插入排序（注意 插入排序从第二个开始）
+3 插入排序 增量为gap 而不是1
+*/
 func main() {
 
-	//https://images2018.cnblogs.com/blog/849589/201803/849589-20180331170017421-364506073.gif
+	//https://upload.wikimedia.org/wikipedia/commons/d/d8/Sorting_shellsort_anim.gif
 	array := []int{6, 7, 9, 3, 6, 8, 1, 9, 3}
 	n := len(array)
 	var gap, i, j int
