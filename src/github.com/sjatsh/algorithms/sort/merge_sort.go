9ac395952d5ba8ea9a7eca85350764928aa854b6
june <i@sjis.me>
2019-12-06 17:26:48 +0800

    1

--- src/github.com/sjatsh/algorithms/sort/merge_sort.go
+++ src/github.com/sjatsh/algorithms/sort/merge_sort.go
@@ -11,43 +11,55 @@ func main() {
 	fmt.Println(array)
 }
 
-func mergeSort(array []int, temp []int, first, last int) {
-
-	if first < last {
-		mid := (first + last) / 2
-		// 左边排序
-		mergeSort(array, temp, first, mid)
-		// 右边排序
-		mergeSort(array, temp, mid+1, last)
-		// 将两个有序序列合并
-		merge(array, temp, first, mid, last)
+// 归并排序：基本思想，数组分为左右两边，对左右两边分别排序，再对两个有序数组进行合并，结束标志：分割的数组只剩下一个元素
+// 1：二分法找出中间值，划分左右两个数组，对左右两个数组分别进行排序
+// 2：重复1继续对数组进行分割，直到只剩下一个元素
+// 3：对每个划分后的有序数组进行合并
+func mergeSort(array []int, temp []int, left, right int) {
+	if left >= right {
+		return
 	}
+	mid := (left + right) / 2
+	// 左边排序
+	mergeSort(array, temp, left, mid)
+	// 右边排序
+	mergeSort(array, temp, mid+1, right)
+	// 将两个有序序列合并
+	merge(array, temp, left, mid, right)
 }
 
-// 将有二个有序数列a[first...mid]和a[mid+1...last]合并。
-func merge(array []int, temp []int, first, mid, last int) {
+// 将有二个有序数列a[left...mid]和a[mid+1...last]合并。
+func merge(array []int, temp []int, left, mid, right int) {
 
-	f := first
-	mid1 := mid + 1
+	// 第一个有序序列第一个下标
+	f := left
+	// 第二个有序序列第一个下标
+	f1 := mid + 1
+
+	// temp结果数组下标
 	k := 0
 
-	for ; f <= mid && mid1 <= last; k++ {
-		if array[f] <= array[mid1] {
+	// 遍历两个有序数组并排序后放入temp数组
+	for ; f <= mid && f1 <= right; k++ {
+		if array[f] <= array[f1] {
 			temp[k] = array[f]
 			f++
-		} else {
-			temp[k] = array[mid1]
-			mid1++
+			continue
 		}
+		temp[k] = array[f1]
+		f1++
 	}
 
+	// 拷贝可能剩余的数据
 	for ; f <= mid; f, k = f+1, k+1 {
 		temp[k] = array[f]
 	}
-	for ; mid1 <= last; mid1, k = mid1+1, k+1 {
-		temp[k] = array[mid1]
+	for ; f1 <= right; f1, k = f1+1, k+1 {
+		temp[k] = array[f1]
 	}
+
+	// 排好序的数据重新放回原数组
 	for i := 0; i < k; i++ {
-		array[first+i] = temp[i]
+		array[left+i] = temp[i]
 	}
 }
